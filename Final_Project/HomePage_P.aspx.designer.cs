a2f390e3240c2b645bec4d9f5744ce2af20c3960
Saurabh.Pathak <saurabh.pathak@nthrive.com>
2023-08-24 19:49:57 +0530

    commit 24 pathak

--- Final_Project/HomePage_P.aspx.designer.cs
+++ Final_Project/HomePage_P.aspx.designer.cs
@@ -114,6 +114,24 @@ namespace Final_Project
         protected global::System.Web.UI.WebControls.Label Label2;
 
         /// <summary>
+        /// Label4 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label4;
+
+        /// <summary>
+        /// Label6 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label6;
+
+        /// <summary>
         /// email control.
         /// </summary>
         /// <remarks>
